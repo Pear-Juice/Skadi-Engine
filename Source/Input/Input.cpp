d98e7d00b12e46d5ed2e3abe0059c759c825387d
PearJuice <pearjuice73@gmail.com>
2024-12-12 17:19:30 -0700

    Added input system

---
+++ Source/Input/Input.cpp
@@ -0,0 +1,5 @@
+//
+// Created by blankitte on 11/19/24.
+//
+
+#include "Input.hpp"
---
+++ Source/Input/Input.hpp
@@ -0,0 +1,16 @@
+//
+// Created by blankitte on 11/19/24.
+//
+
+#ifndef INPUT_HPP
+#define INPUT_HPP
+
+
+
+class Input {
+
+};
+
+
+
+#endif //INPUT_HPP
